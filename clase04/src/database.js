ca3065da208f77d72c3eb602b4d486a50d80f079
Pirvana <pirvanariel@gmail.com>
2019-08-31 04:43:47 -0400

    En esta clase se realizo la creacion de BD y creacion de tareas

---
+++ clase04/src/database.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const URI = process.env.MONGODB_URI;
+
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+})
+    .then(db => console.log('DB is connected'))
+    .catch(err => console.log(err));
